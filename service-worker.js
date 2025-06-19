55835921791328e96a6252ea660c1700f7aa1b0c
Reaper2719 <jdiazr2704@gmail.com>
2025-06-19 15:50:52 -0500

    modulo4 final si es posible

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "modulo4-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "index.html",
+  "style.css",
+  "main.js",
+  "indexedDB.js",
+  "export.js",
+  "manifest.json",
+  "favicon.ico",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
