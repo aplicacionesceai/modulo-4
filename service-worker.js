e4d5f1933fec8613ffbb45d44fab2752ad42c0e5
Reaper2719 <jdiazr2704@gmail.com>
2025-06-26 14:42:36 -0500

    modulo 4 0.0.1

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,16 @@
-const CACHE_NAME = "modulo4-cache-v1";
-const FILES_TO_CACHE = [
-  "/",
-  "index.html",
-  "style.css",
-  "main.js",
-  "indexedDB.js",
-  "export.js",
-  "manifest.json",
-  "favicon.ico",
-  "icon-192.png",
-  "icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('modulo4-store').then(cache => cache.addAll([
+      './',
+      './index.html',
+      './styles.css',
+      './script.js'
+    ]))
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
